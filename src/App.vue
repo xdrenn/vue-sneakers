5be9c5fcd20b2e34422bcc6599fc5c8e69b1a0c4
Anastasia Vilmovskaya <vilmovskaya@gmail.com>
2024-02-22 21:34:07 +0300

    Add shop header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
-<script setup></script>
+<script setup>
+import Header from './components/shop-header.vue'
+</script>
 
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-    <header>
-      <div>
-        <img src="/sneaker-logo.jpg" alt="logo" width="75" height="35" />
-      </div>
-    </header>
-    <h1>Sneakers shop</h1>
-    <p>Old sneakers shop</p>
+    <Header />
   </div>
 </template>
---
+++ src/components/shop-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 py-8 px-10">
+    <div class="flex items-center gap-5">
+      <img src="/sneaker-logo.jpg" alt="logo" width="85" height="45" />
+      <div>
+        <h1 class="text-xl font-bold">SNEAKERS SHOP</h1>
+        <p class="text-gray-500">Vintage sneakers shop</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-5 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="cart" />
+        <b>1200$</b>
+      </li>
+
+      <li class="flex items-center gap-5 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="favourites" />
+        <span>Favourites</span>
+      </li>
+
+      <li class="flex items-center gap-5 text-gray-500 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="profile" />
+        <span>Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
